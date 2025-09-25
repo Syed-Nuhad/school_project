6be183d4e1eb223d01b8475943c58099b5130435
PC <nuhad7july02@gmail.com>
2025-09-26 01:01:00 +0600

    hi

--- core/settings.py
+++ core/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "ui.context_processors.footer_settings",
+                "ui.context_processors.default_class_id",
             ],
         },
     },
